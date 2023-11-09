1c8922f35ef9aa240d4b0e79c7b04eeb5fc368ee
Joaquin Gonzalez  Menza <holaj3415@gmail.com>
2023-11-09 20:45:29 -0300

    falta-id

--- clases.js
+++ clases.js
@@ -1,28 +1,39 @@
 class TicketManager{
     productos = []
-    ultimoProduAñadido =[
-    titulo = "",
-    descripcion = "",
-    precio = 0,
-    imgen = "",
-    code = "",
-    inventario = '']
+    titulo = ""
+    descripcion = ""
+    precio = 0
+    imgen = ""
+    code = 0
+    inventario = ''
     constru(){}
     addProductos(tittle, descrip, pre, img, stock, ){
-        this.ultimoProduAñadido.titulo = tittle, 
-        this.ultimoProduAñadido.descripcion= descrip, 
-        this.ultimoProduAñadido.precio= pre, 
-        this.ultimoProduAñadido.imgen = img, 
-        this.ultimoProduAñadido.inventario = stock, 
-        this.ultimoProduAñadido.code = Math.floor(Math.random()*(100-1)+1)
-
-        this.productos.push(tittle,descrip,pre,img,stock,this.code)
+        if (!this.getProductByCode(this.productos.code)) {
+            this.titulo = tittle, 
+            this.descripcion= descrip, 
+            this.precio= pre, 
+            this.imgen = img, 
+            this.inventario = stock
+            while(this.code < this.productos.length){
+                this.code++;
+            }
+            this.productos.push(tittle,descrip,pre,img,stock,this.code)
+            }else{
+                console.log("El código del producto ya existe.");
+        }
+    }
+    getProductByCode(code){
+        const product = this.productos.find((productos) => productos.code === code);
+        if (product) {
+        return true;
+        }
     }
 }
 
+
 const ticket = new TicketManager
 ticket.addProductos('remeruqui', 'esta re copada esta', 10,'qsy', 98)
-ticket.addProductos('remui', 'esta re copada esta', 10,'qsy', 98)
-ticket.addProductos('ruqui', 'esta re copada esta', 10,'qsy', 98)
-ticket.addProductos('remer', 'esta re da esta', 10,'qsy', 98)
+ticket.addProductos('remui', 'esta re copada', 10,'nas', 98)
+ticket.addProductos('ruqui', 'esta re esta', 10,'opa', 98)
+ticket.addProductos('remer', 'esta re da esta', 10,'se', 98)
 console.log(ticket)
--- mis apuntes/teoria.js
+++
@@ -1,6 +0,0 @@
-class Contador {
-    contructor(nombre){
-        return this.nombre = nombre
-    }
-} 
-const contador = new Contador("Juan") //Juan
